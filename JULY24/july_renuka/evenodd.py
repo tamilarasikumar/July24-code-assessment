48ed31fe7e7650fc3964dcca863283107c720815
Renukachavala476 <chavala.renuka@gmail.com>
2021-07-25 22:43:08 +0530

    even

---
+++ JULY24/july_renuka/evenodd.py
@@ -0,0 +1,11 @@
+evenlist=[]
+oddlist=[]
+n=int(input("enter a number"))
+def evenOdd(n):
+    if(n%2==0):
+        evenlist.append(n)
+    else:
+        oddlist.append(n)
+evenOdd(n)
+print(evenlist)
+print(oddlist)
